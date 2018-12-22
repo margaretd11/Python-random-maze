2165163ab03640b2f7e94797a4a8b923b769d1e1
margaretd11 <duxingran@hotmail.com>
2018-12-22 11:51:01 +0800

    small bug fix

--- random_maze/_init_.py
+++ random_maze/_init_.py
@@ -61,7 +61,7 @@ while 1:
                 doorMap = spanning.createMaze()
                 init(data, doorMap)
                 redrawAll()
-                end()
+                exit = end()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
